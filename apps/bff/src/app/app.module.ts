6d8f26b6982751ed68362830b983e4cbc7880626
Nguyen Van Minh <nvminh162@gmail.com>
2026-04-14 17:08:24 +0700

    chore/#bff: add setup & config env in nestjs

--- apps/bff/src/app/app.module.ts
+++ apps/bff/src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { CONFIGURATION, TConfiguration } from '../configuration';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [ConfigModule.forRoot({ isGlobal: true, load: [() => CONFIGURATION] })],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+    static CONFIGURATION: TConfiguration = CONFIGURATION;
+}
---
+++ apps/bff/src/configuration/index.ts
@@ -0,0 +1,16 @@
+class Configuration {
+    NODE_ENV: string;
+    IS_DEV: boolean;
+    GLOBAL_PREFIX: string;
+    PORT: number;
+
+    constructor() {
+        this.NODE_ENV = process.env.NODE_ENV || 'development';
+        this.IS_DEV = process.env.IS_DEV === 'development';
+        this.GLOBAL_PREFIX = process.env.GLOBAL_PREFIX || 'api/v1';
+        this.PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
+    }
+}
+
+export const CONFIGURATION = new Configuration();
+export type TConfiguration = typeof CONFIGURATION;
