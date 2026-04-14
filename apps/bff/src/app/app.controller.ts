200f4df616c3cbb92707d26713afb1b51b864061
Nguyen Van Minh <nvminh162@gmail.com>
2026-04-14 19:45:33 +0700

    feat/#libs: add responseDTO global to reponse API

--- apps/bff/src/app/app.controller.ts
+++ apps/bff/src/app/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ResponseDTO } from '@common/interfaces/gateway/response.interface';
 
 @Controller('app')
 export class AppController {
@@ -7,6 +8,7 @@ export class AppController {
 
     @Get()
     getData() {
-        return this.appService.getData();
+        const result = this.appService.getData();
+        return new ResponseDTO({ data: result });
     }
 }
