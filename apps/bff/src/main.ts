45b78f0461d714f87472eb493fb0d1d097801b3f
Nguyen Van Minh <nvminh162@gmail.com>
2026-04-14 18:31:06 +0700

    feat/#bff: add swagger base for BFF Gateway

--- apps/bff/src/main.ts
+++ apps/bff/src/main.ts
@@ -6,6 +6,7 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -14,9 +15,27 @@ async function bootstrap() {
     app.setGlobalPrefix(globalPrefix);
     app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
+    /* Config swagger */
+    const config = new DocumentBuilder()
+        .setTitle('E-Invoice BFF API - From @nvminh162')
+        .setDescription('The E-Invoice BFF API description')
+        .setVersion('1.0.0')
+        .addBearerAuth({
+            description: 'Default JWT Authorization',
+            type: 'http',
+            in: 'header',
+            scheme: 'bearer',
+            bearerFormat: 'JWT',
+            name: 'Authorization',
+        })
+        .build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup(`${globalPrefix}/docs`, app, documentFactory);
+
     const port = AppModule.CONFIGURATION.APP_CONFIG.PORT;
     await app.listen(port);
-    Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+    Logger.log(`🚀 +++ Application is running on: http://localhost:${port}/${globalPrefix} +++`);
+    Logger.log(`🚀 +++ Application is running on: http://localhost:${port}/${globalPrefix}/docs +++`);
 }
 
 bootstrap();
